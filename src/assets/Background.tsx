67d1ffa8b1f1b0428e0b115be2f7e7f44f8bb6da
Aaryan <aaryanj@outlook.com>
2025-01-22 14:07:54 -0500

    Update Background.tsx

--- src/assets/Background.tsx
+++ src/assets/Background.tsx
@@ -37,7 +37,7 @@ export function AuroraBackgroundDemo() {
            Using Technology and Business
         </div>
         <div className="bg-black dark:bg-gray-300 rounded-lg w-fit text-white dark:text-black px-4 py-2 text-sm md:text-base">
-          Software Engineering
+          BESc. Software Engineering
         </div>
 
         {/* Icons and Text */}
